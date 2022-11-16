171624f9d55896fc0011e451169559454f2a1a5e
Eduardo Passano Chávez <eapch21@gmail.com>
2022-11-16 17:38:59 -0500

    83. ViewChild - Obtener referencias a objetos del HTML

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-busqueda',
   templateUrl: './busqueda.component.html',
   styleUrls: ['./busqueda.component.css']
 })
-export class BusquedaComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class BusquedaComponent{
+@ViewChild('txtBuscar') txtBuscar!:ElementRef<HTMLInputElement>;
+  buscar(){
+ const valor= this.txtBuscar.nativeElement.value;
+  console.log(valor);
+  this.txtBuscar.nativeElement.value='';
   }
 
 }
