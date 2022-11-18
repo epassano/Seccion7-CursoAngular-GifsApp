ab8ac3c66901c0e2147a89dcc9a70462f57c9a80
Eduardo Passano Chávez <eapch21@gmail.com>
2022-11-18 17:25:51 -0500

    90. LocalStorage

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -15,7 +15,13 @@ export class GifsService {
   get historial() {
     return [...this._historial];
   }
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this._historial=JSON.parse(localStorage.getItem('historial')!)||[]
+    // if(localStorage.getItem('historial')){
+    //   this._historial=JSON.parse(localStorage.getItem('historial')!);
+    // }
+
+  }
 
   buscarGifs(query: string = '') {
     query = query.trim().toLocaleLowerCase();
@@ -23,6 +29,8 @@ export class GifsService {
     if (!this._historial.includes(query)) {
       this._historial.unshift(query);
       this._historial = this._historial.splice(0, 10);
+
+      localStorage.setItem('historial',JSON.stringify(this._historial))
     }
 
     this.http
